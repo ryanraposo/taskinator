e24e0600a23e8d6f2e64536c521890f9ac872251
Ryan Raposo <raposo.ryan@gmail.com>
2022-05-21 08:39:06 -0400

    refactor task creation

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,23 +2,31 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 
-var createTaskHandler = function(event) {
+var taskFormHandler = function(event) {
     event.preventDefault();
 
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+
+    createTaskEl(taskDataObj);
+};
+
+var createTaskEl = function(taskDataObj) {
     var taskItemEl = document.createElement("li");
     taskItemEl.className = "task-item";
 
     var taskInfoEl = document.createElement("div");
     taskInfoEl.className = "task-info";
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
 
     taskItemEl.appendChild(taskInfoEl);
     tasksToDoEl.appendChild(taskItemEl);
-    
 };
 
 
-formEl.addEventListener("submit", createTaskHandler);
+formEl.addEventListener("submit", taskFormHandler);
