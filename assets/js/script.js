ecfd078ce6a28706ba2d4cf849820220c6c7c1d6
Ryan Raposo <raposo.ryan@gmail.com>
2022-05-21 08:32:41 -0400

    add append task

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,19 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var createTaskHandler = function(event) {
     event.preventDefault();
 
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
     var taskItemEl = document.createElement("li");
     taskItemEl.className = "task-item";
-    taskItemEl.textContent = "This is a new task.";
+
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    taskItemEl.appendChild(taskInfoEl);
     tasksToDoEl.appendChild(taskItemEl);
+    
 };
 
 
